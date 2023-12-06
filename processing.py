bce2f2992cd3f6a9791106b8a96f4b87be088dc3
Prayag Krishna <prayagkrishna2001@gmail.com>
2023-12-06 19:30:12 +0530

    Add data processing script to split text file into queries and answers

---
+++ processing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# Read the text file and split the data into interactions
+with open('healthcaremagic_dialogue_4.txt', 'r') as file:
+    data = file.read().split('\nid=')
+
+# Initialize lists to store queries and answers
+queries = []
+answers = []
+
+# Process each interaction and extract 'Description', 'Patient', and 'Doctor' parts
+for curData in data:
+    curData = curData.split('\n')
+    pInd = (curData.index('Patient:'))
+    dInd = (curData.index('Doctor:'))
+    curQuery = curData[4].replace(',',';')
+    for j in range(pInd+1,dInd):
+        curQuery += curData[j].replace(',',';')
+    queries.append(curQuery)
+    
+    curResp=""
+    for j in range(dInd+1,len(curData)):
+        curResp += curData[j].replace(',',';')
+    answers.append(curResp)
+
+
+# Create a DataFrame and save it to a CSV file
+df = pd.DataFrame({'Query': queries, 'Answer': answers})
+df.to_csv('output.csv', index=False)
